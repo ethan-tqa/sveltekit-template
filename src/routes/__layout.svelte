7f17180ea7fde2858611583b53db2316ff1c5f4e
Ethan <ethan.tqa@gmail.com>
2022-05-26 16:04:27 +0800

    v1
    using SvelteKit 1.0.0-next.345

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import '../global.css';
+</script>
+
+<style lang="postcss" global>
+    /* @tailwind base;
+    @tailwind components;
+    @tailwind utilities; */
+</style>
+
+<slot></slot>
